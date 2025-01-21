4c811bf2a096003c2089a2f15e561083eefe10dc
Jedidiah Hernandez <jediboaz27@gmail.com>
2025-01-21 13:44:11 -0800

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -27,18 +27,32 @@ CREATE TABLE HCAHPS_data
     end_date character varying(10)
 )
 
+CREATE TABLE cleaned_hcahps_data AS (
+
 WITH cte_hospital_beds AS (SELECT LPAD(CAST(provider_ccn AS TEXT), 6, '0') AS provider_ccn,
        hospital_name,
        TO_DATE(fiscal_year_begin_date, 'MM/DD/YYYY') AS fiscal_year_begin_date,
        TO_DATE(fiscal_year_end_date, 'MM/DD/YYYY') AS fiscal_year_end_date,
        number_of_beds,
        ROW_NUMBER() OVER(PARTITION BY provider_ccn ORDER BY fiscal_year_begin_date)
-FROM hospital_beds)
+FROM hospital_beds),
 
-SELECT provider_ccn, 
+hospital_beds_cleaned AS (SELECT provider_ccn, 
        hospital_name, 
        fiscal_year_begin_date,
        fiscal_year_end_date,
        number_of_beds
 FROM cte_hospital_beds
-WHERE row_number = 1;
+WHERE row_number = 1),
+
+hcahps_cleaned AS (SELECT LPAD(CAST(facility_id AS TEXT), 6, '0') AS hcahps_provider_ccn,
+        	TO_DATE(start_date, 'MM/DD/YYYY') AS start_date_converted,
+       		TO_DATE(end_date, 'MM/DD/YYYY') AS end_date_converted ,
+	   		*
+FROM hcahps_data)
+
+SELECT * FROM hcahps_cleaned AS hcahps
+LEFT JOIN hospital_beds_cleaned AS beds
+ON hcahps.hcahps_provider_ccn = beds.provider_ccn
+);
+
